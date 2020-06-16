1c1fabb068aaf40b9371079546a4f67295c9fd76
rahulrsinha <rahul.sinha.raw@gmail.com>
2020-06-16 11:55:28 +0530

    Update factorial using tail and head recursion.py

--- Recursion/factorial using tail and head recursion.py
+++ Recursion/factorial using tail and head recursion.py
@@ -10,10 +10,22 @@ print('head recursin',fact_head(6))
 
 
 #tail recursion
+#doesn't need to maintain states.
 
 def fact_head(n,val=1):
     if n<=1:
         return val
+    print(n,val)
     return fact_head(n-1,n*val)
 
+
+#fact_head(5, 1)
+#fact_head(4, 5*1)
+#fact_head(3, 4*5*1)
+#fact_head(2, 3*4*5*1)
+#fact_head(1, 2*3*4*5*1)
+#120
+
 print('tail recursion' , fact_head(6))
+
+
