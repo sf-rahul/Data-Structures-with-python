e6b37a50ae07ddf689117a67db28201f1dd756a7
rahulrsinha <rahul.sinha.raw@gmail.com>
2020-06-18 13:39:14 +0530

    Create roadcutting.py

---
+++ Recursion/roadcutting.py
@@ -0,0 +1,10 @@
+#problem: Given a rope of length n ,you need to find maximum no of pieces you can make,
+#such that the length of every piece is in set {a,b,c} for given three values of a,b,c
+
+#i/p : n=5, a=2,b=5,c=1  o/p = 5
+#i/p : n=23,a=12,b=9,c=11  o/p=2
+#i/p : n=5 , a=4,b=2,c=6 o/p=-1
+
+
+def road_cutting(n):
+    return 
