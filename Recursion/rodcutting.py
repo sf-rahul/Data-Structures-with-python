08dd8d9914483e96c7c8ebf3e1c5f42e16ebac9d
rahulrsinha <rahul.sinha.raw@gmail.com>
2020-06-18 23:24:54 +0530

    Update rodcutting.py

--- Recursion/rodcutting.py
+++ Recursion/rodcutting.py
@@ -5,6 +5,15 @@
 #i/p : n=23,a=12,b=9,c=11  o/p=2
 #i/p : n=5 , a=4,b=2,c=6 o/p=-1
 
+#(n,{a,b,c}) = max((n,a),(n,b),(n,c),(n,a,b),(n,b,c),(n,a,c),(n,a,b,c))
 
-def rod_cutting(n):
-    return 
+def rod_cutting(n,a,b,c):
+    if n<0:
+        return -1
+    if n==0:
+        return 0
+    pieces = max([rod_cutting(n-i,a,b,c) for i in [a,b,c]])
+
+    return -1 if pieces <0 else 1+pieces
+
+print(rod_cutting(23,12,9,11))
