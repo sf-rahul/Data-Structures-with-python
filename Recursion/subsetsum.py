c759502fc5a8f33fa2af1166a9e8bec8b5aa40b4
sf-rahul <58662401+sf-rahul@users.noreply.github.com>
2020-07-27 11:00:50 +0530

    Update subsetsum.py

--- Recursion/subsetsum.py
+++ Recursion/subsetsum.py
@@ -1,31 +1,5 @@
-# cook your dish here
-t = int(input())
-while t>0:
-    n = int(input())
-    lst = list(map(int,input().split()))
-    d= {}
-    l = []
-    q= False
-    for i in range(0,n):
-        k = [0]*n
-        k[i]=lst[i]|lst[i]
-        if k[i] in d:
-            q=True
-            break
-        d[k[i]]=1
-        l.append(k)
-    if q==False:
-        for i in range(n-2,-1,-1):
-            for j in range(i+1,n):
-                l[i][j]=l[i][j-1]|lst[j]
-                if l[i][j] in d:
-                    q=True
-                    break
-                d[l[i][j]] =1
-    if not q:
-        print('YES')
-    else:
-        print('NO')
-    #print(len(d.keys()))
-            
-    t = t-1
+def subsetsum(arr,n,s):
+    if n==0:
+        return 1 if s==0 else 0
+    return subsetsum(arr,n-1,s) + subsetsum(arr,n-1,s-arr[n-1])
+print(subsetsum([5,3,2,6],4,8))
